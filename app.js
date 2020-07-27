633d95c36b73d1ea7a3686f66b096033ec10adb6
smardone <shaun.mardones@gmail.com>
2020-07-26 22:16:55 -0400

    modulized code

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
 
-    console.log("================");
+fs.writeFile("index.html", generatePage(name, github), err => {
+    if (err) throw err;
 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-    
-};
-
-printProfileData(profileDataArgs);
+    console.log("Portfolio complete! Checkout index.htm to see the output!")
+})
