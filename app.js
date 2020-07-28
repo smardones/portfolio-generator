e06f1369736e7007148dd9ecefa33c747980d33e
smardone <shaun.mardones@gmail.com>
2020-07-27 21:11:54 -0400

    added inquirer package

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+const inquirer = require("inquirer");
 
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if (err) throw err;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-    console.log("Portfolio complete! Checkout index.htm to see the output!")
-})
+
+// const pageHTML = generatePage(portfolioData));
+
+// fs.writeFile("index.html", generatePage(name, github), err => {
+//     if (err) throw err;
+
+//     console.log("Portfolio complete! Checkout index.htm to see the output!")
+// })
